1fa54880dabf4e7fb4e2ca9d4e4ef79f7166a192
Yandry Alvarez <wixy122018@gmail.com>
2025-10-05 18:09:28 -0500

    Add files via upload

---
+++ JUEGO PIEDRA-PAPEL.py
@@ -0,0 +1,33 @@
+import random
+#BUCLE DE INICIO DE JUEGO
+while True:
+
+ print("Bienvenido al juego Piedra, Papel o Tijera")
+
+#LISTA DE JUEGO
+ opciones=["Piedra","Papel","Tijera"]
+
+ jugador= input("Elige Piedra, Papel o Tijera: ").title()
+ computadora=(random.choice(opciones))
+
+ print("jugador:", jugador)
+ print("computadora:", computadora)
+
+#CONDICIONES DE JUEGO
+ if jugador == computadora:
+    print("Empate!")
+ elif (jugador == "Piedra" and computadora == "Tijera") or (jugador == "Papel" and computadora == "Piedra") or (jugador == "Tijera" and computadora == "Papel"):
+    print("¡Ganaste!")
+ else:
+    print("¡Perdiste!")
+
+#REINICIAR JUEGO
+ if input("Jugamos Otra vez? (si/no): ").title() == "Si":
+    print("Vamos de nuevo")
+    continue
+ else:
+    print("Gracias por jugar")
+    break
+    
+
+    
